558c677d58a8fa3ed15d7cbde2466057292cca2d
Don Wells <jdonwells@gmail.com>
2022-04-05 17:13:43 +0000

    New design.

--- main.py
+++ main.py
@@ -1,200 +1,382 @@
 """
-Text Monster Game
+Text Adventure Game
 
-The goal of this game is to beat the monsters and claim the prize at the end of the dungeon.
+The goal of this game is to beat the monsters and claim the prize at the end of 
+the dungeon.
+"""
+
+"""
+The map and descriptions of various rooms and items. 5 character abbreviations so you
+can see what the map is easily. None is used to indicate a solid wall. I have put None
+around the entire map so I don't have to code exceptions for off the map indexing.
 """
 
 dungeon_map = [
-    ["empty", "sword", "stairs up", "monster", "empty"],
-    ["magic stones", "monster", "stairs down", "empty", "stairs up"],
-    ["prize", "boss monster", "sword", "sword", "stairs down"],
+    [None,  None,    None,    None,    None,    None,    None,    None,    None,   None],
+    [None, "start", "sword", "empty", "tapes", "plant", "sword", "orc",   "empty", None],
+    [None,  None,    None,   "tarot",  None,    None,   "maze",   None,    None,   None],
+    [None, "kitch", "sword", "orc",    None,   "maze",  "maze",  "maze",  "maze",  None],
+    [None, "leavs",  None,    None,    None,   "maze",   None,   "maze",  "maze",  None],
+    [None, "tapes",  None,   "plant",  None,   "spidr", "maze",   None,   "maze",  None],
+    [None, "libry", "tunlN", "store",  None,    None,    None,   "maze",  "maze",  None],
+    [None,  None,   "tunnl",  None,    None,   "halbd", "mazOW", "maze",   None,   None],
+    [None, "sword", "tunlS", "puddl",  None,    None,   "hole",   None,    None,   None],
+    [None,  None,    None,   "bigNW", "bigNE", "plant", "kitch", "troll", "tapes", None],
+    [None, "stnes", "thron", "bigSW", "bigSE", "sword",  None,    None,   "empty", None],
+    [None,  None,    None,   "troll",  None,    None,    None,   "prize", "troll", None],
+    [None,  None,    None,    None,    None,    None,    None,    None,    None,   None],
 ]
 
-roomDescriptions = {
-    "empty": "You are in an empty room.",
-    "sword": "There is a sword here!",
-    "stairs up": "There are stairs going up.",
-    "stairs down": "You see stairs going down.",
-    "monster": "There is an angry monster here!",
-    "boss monster": "There is an extraordinarily large unhappy monster here.",
-    "corpse": "This room has a foul smelling puddle.",
-    "magic stones": "There are mysterious glowing stones here.",
+room_descriptions = {
+    # things you can pick up
+    "sword": "This is an unremarkable room. There is a rusty old sword here!",
+    "halbd": "So many rusted and broken weapons. There is one that stands out. "
+             "A halberd decorated with gold and silver that glows with some "
+             "kind of magic.",
+    "stnes": "There are mysterious glowing stones here.",
     "prize": "You have found a prize!",
+    # an empty room for after you pick up the thing
+    "empty": "You are in an empty room.",
+    # some different rooms that just have discriptions and can be used and reused
+    "start": "You are stuck at the bottom of a dry well. There is a moldy" 
+             " door to the East that you forced open.",
+    "thron": "You are in a throne room complete with a large gaudy chair. "
+             "You imagine it was resplendent in gold and red before it all "
+             "faded to brown and grey.",
+    "tapes": "You have found a room with large faded tapestries on the walls. "
+             "Moths have chewed holes in some of them.",
+    "puddl": "A foul smelling puddle makes you feel a bit ill.",
+    "kitch": "This room has a very large cauldron over a burnt out fire pit. "
+             "From the bones piled neatly in the corner you surmise adventurers"
+             " are cooked and eaten here.",
+    "tarot": "A goblin with gaudy makeup is sitting at a table with tarot cards"
+             " laid out in front of her. She looks up as you enter. She flips a "
+             'card and gives you your fortune: "Death".',
+    "store": "This was once a room full of crates and barrels to store "
+             "important things. Everything has been broken open and emptied. "
+             "What a mess.",
+    "libry": "This library is in poor condition. The books crumble to dust when "
+             "you touch them. Ironically, as you pick up a novel by Margaret "
+             "Mitchell it turns to dust and blows away in a cool draft of air.",
+    # a tight tunnel that takes up 3 spaces
+    "tunlN": "A small tunnel goes South. You might be able to squeeze through.",
+    "tunnl": "You are in a tunnel just big enough to squeeze through.",
+    "tunlS": "A small tunnel goes North. You might be able to squeeze through.",
+    # The maze uses the same discription over and over to confuse the player.
+    # There is a way to get out with the halberd and get to the prize.
+    "maze" : "You are in a maze of twisty little passages, all alike.",
+    "mazOW": "You are in a maze of twisty little passages. There is a hole in"
+             " the wall to the South with a ladder to reach it. If you go "
+             "through it you may not get back.",
+    "hole" : "There is a hole near the ceiling to the North, if you stack a few "
+             "dead bodies up you might be able to reach it.",
+    # a big cave that takes up 4 locations on the map. It discribes an orc troll war.
+    "bigNW": "This is an enormous cave. It looks like a war between orcs and "
+             "trolls has been going on here for a while. You are in the North"
+             " West corner.",
+    "bigNE": "This is an enormous cave. The war between orcs and trolls has "
+             "been brutal. You are in the North East corner.",
+    "bigSW": "This is an enormous cave. Dead orcs and trolls are every where. "
+             "You are in the South West corner.",
+    "bigSE": "This is an enormous cave. It looks like a war between orcs and "
+             "trolls started here. You are in the South East corner.",
+    # monsters that want to kill you.
+    "orc"  : "There is an angry orc here! He is going to kill you without "
+             "any further discussion.",
+    "troll": "An extraordinarily large, unhappy troll is sitting here. He "
+             "isn't going to let you pass without extracting a cost.",
+    "plant": "There is a huge carnivorous plant growing out of a large crack "
+             "in the floor. It is snapping at you. You could try sneaking around "
+             "it, but it will probably eat you.",
+    "spidr": "A very large spider sits motionless in an even larger web.",
+    # dead versions of all the monsters that didn't kill you.
+    "corpse": "You find a dead orc just laying there.",
+    "troll corpse": "A dead troll is sprawled out across the room. "
+                "You will need to climb over him.",
+    "leavs": "This room is littered with very large dried up leaves.",
+    "gooey web": "There is a large spider web with some kind of goo all over it.",
 }
 
-monsters = ("monster", "boss monster")
-
-roomContents = {
-    "sword": "+5 magic sword",
-    "magic stones": "some mysterious glowing stones",
+item_descriptions = {
+    "sword": "a rusty sword",
+    "stnes": "some mysterious glowing stones",
+    "halbd": "a glowing ceremonial halberd",
     "prize": "The Final Prize",
 }
+
+death_by_monster = {
+    "orc"  : "The orc kills you in a most horrible way.",
+    "troll": "You are unprepared. The surprisingly big troll cheerfully kills you.",
+    "plant": "You are grabbed by the large plant and slowly digested over the next "
+             "couple of days.",
+    "spidr": "The spider grabs you with amazing speed and agility. You will be "
+             "eaten in a few days. Enjoy hanging out with the spider.",
+}
+
+kill_the_monster = {
+    "orc"  : "You manage to kill the orc.",
+    "troll": "The surprisingly big troll also dies surprisingly easy.",
+    "plant": "You hack the plant into pieces. The large leaves are "
+             "already turning brown.",
+    "spidr": "You stab the overgrown spider and it oozes in a disgusting way.",
+}
+
+monster_to_corpse = {
+    "orc"  : "corpse",
+    "troll": "troll corpse",
+    "plant": "leavs",
+    "spidr": "gooey web",
+}
+
+"""
+Room locations and code to manipulate them.
+
+A room location is a list of two integers. The first or 0 element
+is the north-south index. The second or 1 element is the east-west
+index.
+"""
+
+moves = {
+        "east" : [0, 1], 
+        "west" : [0, -1], 
+        "north": [-1, 0], 
+        "south": [1, 0]
+    }
+
+
+def start_location():
+    return [1, 1]
+
+
+def change_room(direction):
+    return [room[0] + moves[direction][0], room[1] + moves[direction][1]]
+
+
+def room_type(here):
+    return dungeon_map[here[0]][here[1]]
+
+
+def this_room():
+    return room_type(room)
+
+
+def set_room_type(type):
+    global dungeon_map
+    dungeon_map[room[0]][room[1]] = type
+
+    
+def room_exists(a_room):
+    return room_type(a_room) != None
+
+
+def empty_this_room():
+    set_room_type("empty")
+
+
+def description():
+    return room_descriptions[this_room()]
+
+
+def monster_died_here():
+    set_room_type(monster_to_corpse[this_room()])
+
+
+"""
+Functions to run the various commands you can enter.
+"""
 
 
 def help():
     print(
-        "You may go left (l), right (r), up or down. You can get and fight. You may also check your bag."
+        "You may go West (w), East (e), up (u) or down (d) if there is an exit.",
+        "You can get (g) and fight (f). You may also check your bag (b).",
+        "To repeat the room description use look (l). To repeat this type help.",
+        "\n"
     )
 
 
-def left():
-    move("left")
+def west():
+    move("west")
 
 
-def right():
-    move("right")
+def east():
+    move("east")
 
 
-def move(direction):
-    global previous_room, room, state
-    room_change = {"left": -1, "right": 1}
-    there_is_a_monster = this_room() in monsters
-    going_back = room + room_change[direction] == previous_room
-    last_room = (first_room_on_this_floor(floor)
-                 if direction == "left" else last_room_on_this_floor(floor))
-    if there_is_a_monster and not going_back:
-        print("The monster won't let you pass! You have died trying.")
-        state = "lose"
-        return
-    if room == last_room:
-        print(f"You can't go {direction}.")
-        return
-    previous_room = room
-    room += room_change[direction]
+def north():
+    move("north")
 
 
-def up():
-    global floor
-    if this_room() == "stairs up":
-        floor += 1
-    else:
-        print("There are no stairs going up.")
+def south():
+    move("south")
 
 
-def down():
-    global floor
-    if this_room() == "stairs down":
-        floor -= 1
-    else:
-        print("There are no stairs going down.")
+def look():
+    print(description())
+    print(exits())
 
 
 def get():
     global state
-    if this_room() not in roomContents:
+    if this_room() not in item_descriptions:
         print("There is nothing to get.")
-        return
-    if len(inventory) >= 3:
-        print("You can only carry three things.")
-        return
-    print("You get {}".format(roomContents[this_room()]))
-    if this_room() == "prize":
-        state = "win"
-    inventory.append(this_room())
-    empty_this_room(floor, room)
+    else:
+        print("You get " + item_descriptions[this_room()] + ".")
+        if this_room() == "prize":
+            state = "win"
+        bag.append(this_room())
+        empty_this_room()
 
 
 def fight():
     global state
-    ready_for_monster = "sword" in inventory
-    ready_for_boss = ready_for_monster and "magic stones" in inventory
-    monster_here = this_room() == "monster"
-    boss_here = this_room() == "boss monster"
-    if not monster_here and not boss_here:
+    if not there_is_a_monster_here():
         print("Relax, there is nothing to fight here.")
-        return
-    if monster_here and not ready_for_monster:
-        print("The monster kills you in a most horrible way.")
+    elif (
+        (there_is_a_monster_here() and not ready_for_monster()) or 
+        (there_is_a_troll_here() and not ready_for_troll())
+    ):  # even Python is sad about it
+        print(death_by_monster[this_room()])
         state = "lose"
-        return
-    if boss_here and not ready_for_boss:
-        print(
-            "You are unprepared. The surprisingly big monster cheerfully kills you."
-        )
-        state = "lose"
-        return
-    print(
-        "You slay the evil beast and it dissolves. Unfortunately, your sword also dissolves."
-    )
-    inventory.remove("sword")
-    monster_died_here(floor, room)
+    else:
+        print(kill_the_monster[this_room()])
+        if not "halbd" in bag:
+            print("Your rusty old sword breaks. You won't be using it again.")
+            bag.remove("sword")
+        monster_died_here()
 
 
-def bag():
-    if len(inventory) == 0:
+def bag_contents():
+    if len(bag) == 0:
         print("Your bag is empty.")
-        return
-    print(", ".join([roomContents[item] for item in inventory]))
+    else:
+        for item in bag:
+            print(item_descriptions[item])
 
 
-def this_room():
-    return dungeon_map[floor][room]
+def unknown():
+    print('Command not recognized. Type "help" to see commands.')
 
 
-def first_room_on_this_floor(__):
-    return 0
+"""
+Functions that help other functions in some way.
+"""
 
 
-def last_room_on_this_floor(floor):
-    global dungeon_map
-    return len(dungeon_map[floor]) - 1
+def move(direction):
+    global previous_room, room, state
+    new_room = change_room(direction)
+    going_back = new_room == previous_room
+    if not room_exists(new_room):
+        print("You can't go that way.")
+    elif there_is_a_monster_here() and not going_back:
+        print("The monster won't let you pass!", death_by_monster[this_room()])
+        state = "lose"
+    else:
+        previous_room = room
+        room = new_room
+        look()
 
 
-def empty_this_room(floor, room):
-    global dungeon_map
-    dungeon_map[floor][room] = "empty"
+def exits():
+    good_exits = [
+        direction for direction in ["east", "west", "north", "south"] 
+        if room_exists(change_room(direction))
+    ]
+    description = "You can go "
+    if len(good_exits) == 1:
+        description = description + good_exits[0]
+    else:
+        for direction in good_exits[0:-1]:
+            description = description + direction + ", "
+        description = description + "or " + good_exits[-1]
+    return description + "."
 
 
-def description(floor, room):
-    return roomDescriptions[dungeon_map[floor][room]]
+def there_is_a_monster_here():
+    return this_room() in death_by_monster
 
 
-def monster_died_here(floor, room):
-    global dungeon_map
-    dungeon_map[floor][room] = "corpse"
+def there_is_a_troll_here():
+    return this_room() == "troll"
 
+
+def ready_for_monster():
+    return "sword" in bag or "halbd" in bag
+
+
+def ready_for_troll():
+    return ("sword" in bag and "stnes" in bag) or "halbd" in bag
+
+
+def initialize():
+    global bag, room, previous_room, state
+    bag = []
+    room = start_location()
+    previous_room = start_location()
+    state = "playing"
+
+
+def print_winner():
+    # Way back before digital images there was ASCII art.
+    line4 = (" "*3+"/ / //"+" "*3+"// //"+" "*3+"//"+" "*4+"||/ / |  / " +
+             "// //"+" "*3+"//")
+    line7 = " "*21+"/| "+"_"*16
+    line3 = ("  \\\\/ / //"+" "*3+")) //"+" "*3+"//"+" "*3+"|| / /||/ " +
+             "/ // //"+" "*3+"))  //")
+    line1 = "\\\\"+" "*4+"/ /"+" "*17+"||"+" "*3+"/ |  / /"+" "*13+"//"
+    line5 = (" "*2+"/ / (("+"_"*3+"// (("+"_"*3+"(("+" "*5+"|  /  | / /" +
+             "/ //"+" "*3+"//  //")
+    line8 = " "*15+"O|"+"="*3+"|* >"+"_"*16+">"
+    line2 = " \\\\  / /  ___"+" "*13+"||  /  | / "+"/ ()  ___"+" "*5+"//"
+    line9 = " "*21+"\|"
+    if state == "win":
+        print(
+            "", "", line1, line2, line3, line4, line5, 
+            "", line7, line8, line9,
+            "", "", "You have won the game.", 
+            sep="\n"
+        )
+    else:
+        print("You lost the game. Maybe next time.")
+
+
+"""
+A dictionary that translates a command into a function that needs to be called.
+To add new commands just add the command's name as a key and a function name 
+as the value.
+"""
 
 commands = {
     "help": help,
-    "left": left,
-    "l": left,
-    "right": right,
-    "r": right,
-    "up": up,
-    "u": up,
-    "down": down,
-    "d": down,
-    "get": get,
-    "fight": fight,
-    "bag": bag,
+    "west": west, "w": west,
+    "east": east, "e": east,
+    "south": south, "s": south,
+    "north": north, "n": north,
+    "get": get, "g": get,
+    "fight": fight, "f": fight,
+    "bag": bag_contents, "b": bag_contents,
+    "look": look, "l": look,
 }
 
 
-def playGame():
+def welcome():
+    print("Welcome to my Text Adventure Game. Kill the monsters, survive, and find the Final Prize to win!\n")
+
+
+def play_game():
     """
-    Start up my text monster game. Everything is printed to stdout. Input is from stdin.
+    Start up my text adventure game. Uses the console for everything.
     """
-
-    global inventory, floor, room, previous_room, state
-
-    inventory = []
-    floor = 0
-    room = 0
-    previous_room = 0
-    state = "running"
-
-    while state != "win" and state != "lose":
-        print(description(floor, room))
-        command = input("What will you do? ")
-        if command in commands:
-            commands[command]()
-        else:
-            print('Command not recognized. Type "help" to see commands.')
-
-    if state == "win":
-        print("You won the game! :)")
-    else:
-        print("You lost the game. :( Maybe next time.")
+    initialize()
+    welcome()
+    help()
+    look()
+    while state == "playing":
+        # Process commands with a dictionary. If the command is in the dictionary
+        # call the value as a function. If not call the default function unknown().
+        commands.get(input("What will you do? "), unknown)()
+    print_winner()
 
 
-playGame()
+play_game()
